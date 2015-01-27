f74ad11979fbc0ada3d1d1e5d3400e3464501c42
Rost Khanyukov <rost.khanyukov@gmail.com>
2015-01-27 17:11:24 +0200

    Initial

---
+++ js/main.js
@@ -0,0 +1,124 @@
+var tasks = [
+	{img: 1, answer: "b"}, 
+	{img: 2, answer: "a"},
+	{img: 3, answer: "b"},
+	{img: 4, answer: "d"},
+	{img: 5, answer: "e"},
+	{img: 6, answer: "d"},
+	{img: 7, answer: "b"},
+	{img: 8, answer: "d"},
+	{img: 9, answer: "e"},
+	{img: 10, answer: "c"},
+	{img: 11, answer: "e"},
+	{img: 12, answer: "c"},
+	{img: 13, answer: "e"},
+	{img: 14, answer: "b"},
+	{img: 15, answer: "d"},
+	{img: 16, answer: "c"},
+	{img: 17, answer: "a"},
+	{img: 18, answer: "b"},
+	{img: 19, answer: "e"},
+	{img: 20, answer: "c"},
+	{img: 21, answer: "d"},
+	{img: 22, answer: "e"},
+	{img: 23, answer: "d"},
+];
+
+var missedTasks = [];
+
+function loadEndScreen() {
+	$(".inner").html("<h1>Finish</h1><img style=\"width:400px;\" src=\"img/finish.jpg\">");
+}
+
+function getCookie(name) {
+	var matches = document.cookie.match(
+		new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)")
+	);
+	return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function wrongAnswer(i) {
+	var answers = ["Неправильно", "Знову неправильно", "І вкотре неправильно"];
+	$("#answer").css("color", "red").text(" - " + answers[i % 3] + "!");
+}
+
+function notAnswer() {
+	$("#answer").css("color", "yellow").text(" - спершу оберіть варіант!");
+}
+
+function setCookie(name, value, options) {
+	options = options || {};
+	var expires = options.expires;
+
+	if (typeof expires == "number" && expires) {
+		var d = new Date();
+		d.setTime(d.getTime() + expires * 1000);
+		expires = options.expires = d;
+	}
+	if (expires && expires.toUTCString) {
+		options.expires = expires.toUTCString();
+	}
+
+	value = encodeURIComponent(value);
+	var updatedCookie = name + "=" + value;
+
+	for (var propName in options) {
+		updatedCookie += "; " + propName;
+		var propValue = options[propName];
+		if (propValue !== true) {
+			updatedCookie += "=" + propValue;
+		}
+	}
+	document.cookie = updatedCookie;
+}
+
+function load(imgId) {
+	$("#taskImg").attr("src", "img/" + imgId + ".PNG");
+	$("#num").text(imgId);
+	$("#answer").text("");
+}
+
+$(document).ready(function() {
+	var taskId = getCookie("taskId");
+	var wrongAnswerId = 0;
+	
+	if (taskId == undefined) {
+		taskId = 0;
+	} else {
+		load(tasks[taskId].img);
+	}
+
+	$("#button").click(function() {
+		checkedRadio = $("input[name=answer]:checked");
+		userAns = checkedRadio.val();
+		
+		if (userAns == undefined) {
+			notAnswer();
+			return false;
+		}
+		
+		if (userAns != tasks[taskId].answer) {
+			wrongAnswer(wrongAnswerId++);
+			return false;
+		} 
+		checkedRadio.prop('checked', false);
+
+		taskId++;
+		if (taskId + 1 >= tasks.length) {
+			if (missedTasks.length == 0) {
+				loadEndScreen();
+				setCookie("taskId", 0);
+			} else {
+				load(tasks[missedTasks.shift()].img);
+			}
+		} else {
+			load(tasks[taskId].img);
+			setCookie("taskId", taskId);
+		}
+	});
+	
+	$("#miss").click(function() {
+		missedTasks.push(taskId);
+		load(tasks[++taskId].img);
+	});
+});
