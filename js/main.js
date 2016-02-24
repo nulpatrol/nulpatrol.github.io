1b6e048428677ea3de0638f10f8c62442a0daacd
Rost Khaniukov <rost.khanyukov@gmail.com>
2016-02-24 09:37:06 +0200

    Drop nah folder

--- js/main.js
+++ js/main.js
@@ -1,54 +1,3 @@
-function loadEndScreen() {
-	$(".inner").html("<h1>Finish</h1><img style=\"width:400px;\" src=\"img/finish.jpg\">");
-}
-
-function getCookie(name) {
-  var matches = document.cookie.match(new RegExp(
-    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-  ));
-  return matches ? decodeURIComponent(matches[1]) : undefined;
-}
-
-function wrongAnswer(i) {
-	var answers = ["Неправильно", "Знову неправильно", "І вкотре неправильно"];
-	$("#answer").text(" - " + answers[i % 3] + "!");
-}
-
-function setCookie(name, value, options) {
-  options = options || {};
-
-  var expires = options.expires;
-
-  if (typeof expires == "number" && expires) {
-    var d = new Date();
-    d.setTime(d.getTime() + expires * 1000);
-    expires = options.expires = d;
-  }
-  if (expires && expires.toUTCString) {
-    options.expires = expires.toUTCString();
-  }
-
-  value = encodeURIComponent(value);
-
-  var updatedCookie = name + "=" + value;
-
-  for (var propName in options) {
-    updatedCookie += "; " + propName;
-    var propValue = options[propName];
-    if (propValue !== true) {
-      updatedCookie += "=" + propValue;
-    }
-  }
-
-  document.cookie = updatedCookie;
-}
-
-function load(imgId) {
-	$("#taskImg").attr("src", "img/" + imgId + ".PNG");
-	$("#num").text(imgId);
-	$("#answer").text("");
-}
-
 var tasks = [
 	{img: 1, answer: "b"}, 
 	{img: 2, answer: "a"},
@@ -75,6 +24,60 @@ var tasks = [
 	{img: 23, answer: "d"},
 ];
 
+var missedTasks = [];
+
+function loadEndScreen() {
+	$(".inner").html("<h1>Finish</h1><img style=\"width:400px;\" src=\"img/finish.jpg\">");
+}
+
+function getCookie(name) {
+	var matches = document.cookie.match(
+		new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)")
+	);
+	return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function wrongAnswer(i) {
+	var answers = ["Неправильно", "Знову неправильно", "І вкотре неправильно"];
+	$("#answer").css("color", "red").text(" - " + answers[i % 3] + "!");
+}
+
+function notAnswer() {
+	$("#answer").css("color", "yellow").text(" - спершу оберіть варіант!");
+}
+
+function setCookie(name, value, options) {
+	options = options || {};
+	var expires = options.expires;
+
+	if (typeof expires == "number" && expires) {
+		var d = new Date();
+		d.setTime(d.getTime() + expires * 1000);
+		expires = options.expires = d;
+	}
+	if (expires && expires.toUTCString) {
+		options.expires = expires.toUTCString();
+	}
+
+	value = encodeURIComponent(value);
+	var updatedCookie = name + "=" + value;
+
+	for (var propName in options) {
+		updatedCookie += "; " + propName;
+		var propValue = options[propName];
+		if (propValue !== true) {
+			updatedCookie += "=" + propValue;
+		}
+	}
+	document.cookie = updatedCookie;
+}
+
+function load(imgId) {
+	$("#taskImg").attr("src", "img/" + imgId + ".PNG");
+	$("#num").text(imgId);
+	$("#answer").text("");
+}
+
 $(document).ready(function() {
 	var taskId = getCookie("taskId");
 	var wrongAnswerId = 0;
@@ -88,10 +91,13 @@ $(document).ready(function() {
 	$("#button").click(function() {
 		checkedRadio = $("input[name=answer]:checked");
 		userAns = checkedRadio.val();
-		console.log(userAns);
-		console.log(tasks[taskId].answer);
+		
+		if (userAns == undefined) {
+			notAnswer();
+			return false;
+		}
+		
 		if (userAns != tasks[taskId].answer) {
-			console.log("wrong");
 			wrongAnswer(wrongAnswerId++);
 			return false;
 		} 
@@ -99,10 +105,20 @@ $(document).ready(function() {
 
 		taskId++;
 		if (taskId + 1 >= tasks.length) {
-			loadEndScreen();
-			setCookie("taskId", 0);
+			if (missedTasks.length == 0) {
+				loadEndScreen();
+				setCookie("taskId", 0);
+			} else {
+				load(tasks[missedTasks.shift()].img);
+			}
+		} else {
+			load(tasks[taskId].img);
+			setCookie("taskId", taskId);
 		}
-		load(tasks[taskId].img);
-		setCookie("taskId", taskId);
+	});
+	
+	$("#miss").click(function() {
+		missedTasks.push(taskId);
+		load(tasks[++taskId].img);
 	});
 });
