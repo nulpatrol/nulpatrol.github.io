531b276f55fea833bce2daa8be0a63f49f62bc7c
Rost Khaniukov <rost.khanyukov@gmail.com>
2016-03-02 17:41:53 +0200

    Add start page

--- js/main.js
+++ js/main.js
@@ -1,4 +1,73 @@
 var answers = "dcaeecedadedbcddebdba";
+
+var topics = [
+	{ "id" : 1, "theme": "Похідні" },
+	{ "id" : 2, "theme": "Інтеграли" },
+];
+
+function makeSelect(options, header) {
+	var newSelect = $('<select />');
+	newSelect.appendTo(".inner");
+	
+	$('<option />', { value: "hide", text: header }).appendTo(newSelect);
+	
+	options.forEach(function(item, i, options) {
+		$('<option />', { value: item.id, text: item.theme }).appendTo(newSelect);
+	});
+	
+	styleSelect(newSelect);
+}
+
+function selectChange(value, id) {
+	alert(id);
+}
+
+function styleSelect(scope) {
+	var $this = $(scope), numberOfOptions = $(scope).children('option').length;
+	console.log(numberOfOptions);
+    $this.addClass('select-hidden'); 
+    $this.wrap('<div class="select"></div>');
+    $this.after('<div class="select-styled"></div>');
+
+    var $styledSelect = $this.next('div.select-styled');
+    $styledSelect.text($this.children('option').eq(0).text());
+  
+    var $list = $('<ul />', {
+        'class': 'select-options'
+    }).insertAfter($styledSelect);
+  
+    for (var i = 0; i < numberOfOptions; i++) {
+        $('<li />', {
+            text: $this.children('option').eq(i).text(),
+            rel: $this.children('option').eq(i).val()
+        }).appendTo($list);
+    }
+  
+    var $listItems = $list.children('li');
+  
+    $styledSelect.click(function(e) {
+        e.stopPropagation();
+        $('div.select-styled.active').each(function(){
+            $(this).removeClass('active').next('ul.select-options').hide();
+        });
+        $(this).toggleClass('active').next('ul.select-options').toggle();
+    });
+  
+    $listItems.click(function(e) {
+        e.stopPropagation();
+        $styledSelect.text($(this).text()).removeClass('active');
+        $this.val($(this).attr('rel'));
+		selectChange($this.val(), $this.attr("id"));
+        $list.hide();
+    });
+  
+    $(document).click(function() {
+        $styledSelect.removeClass('active');
+        $list.hide();
+    });
+
+}
+
 var tips = [
 	"Сума двох сторін трикутника завжди більша, ніж третя сторона. УВАГА: це потрібно перевірити для кожної сторони",
 	"Сума кутів трикутника - 180 градусів",
@@ -56,6 +125,9 @@ function load(tid, theme) {
 }
 
 $(document).ready(function() {
+	
+	makeSelect(topics, "-- Оберіть тему --")
+	
 	var wrongAnswerId = 0;
 	var params = parseHash();
 	var theme = params[0];
