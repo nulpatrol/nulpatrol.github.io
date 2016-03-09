be62c030d0fbdab98dc383ade1140e41df286647
Rost Khanyukov <rost.khanyukov@gmail.com>
2016-03-09 21:10:45 +0200

    Add theme #31 answers

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var answers = "dcdeaaebdeaecaebbdcbdbb";
+var answers = "dceadbeccacadcadedbb";
 var tips = [
 	"Сума двох сторін трикутника завжди більша, ніж третя сторона. УВАГА: це потрібно перевірити для кожної сторони",
 	"Сума кутів трикутника - 180 градусів",
